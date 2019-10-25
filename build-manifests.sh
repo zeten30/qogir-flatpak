8bf2df020416c3ef616c0ca43cff0a3d8c92a815
Milan Zink <mzink@redhat.com>
2019-10-25 13:07:46 +0200

    Update to Qogir release 2019-10-25

--- build-manifests.sh
+++ build-manifests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FLATPAK_PREFIX=org.gtk.Gtk3theme
-RELEASE_TAR=2019-08-31.tar.gz
-RELEASE_SHA256=e2c26c29d8a72bc67bcd13934cc1ef95587295a5dbc50dd61f047891a75e3b4d
+RELEASE_TAR=Qogir-theme-2019-10-25.tar.gz
+RELEASE_SHA256=07027ad930f4cba10c92ce782fa6b3747afefaafc4bc029692e95ad8f3dcd1ff
 BRANCH=3.22
 RUNTIME_V=3.26
 THEME_VARIANTS="Qogir
@@ -25,7 +25,6 @@ Qogir-win-dark
 Qogir-win-light"
 
 
-
 for th in ${THEME_VARIANTS}; do
   # Copy templates
   cp templates/appdata.template.xml "${FLATPAK_PREFIX}.${th}.appdata.xml"
@@ -38,9 +37,6 @@ for th in ${THEME_VARIANTS}; do
   sed -i "s/{{release_sha256}}/${RELEASE_SHA256}/g" "${FLATPAK_PREFIX}.${th}.appdata.xml" "${FLATPAK_PREFIX}.${th}.json"
   sed -i "s/{{branch}}/${BRANCH}/g" "${FLATPAK_PREFIX}.${th}.appdata.xml" "${FLATPAK_PREFIX}.${th}.json"
   sed -i "s/{{runtime_v}}/${RUNTIME_V}/g" "${FLATPAK_PREFIX}.${th}.appdata.xml" "${FLATPAK_PREFIX}.${th}.json"
-
-  # Build in local-repo
-  flatpak-builder --repo=local-repo --force-clean build "${FLATPAK_PREFIX}.${th}.json"
 done
 
 # Show local-repo installation
@@ -49,6 +45,9 @@ echo "## Installation ##"
 echo "flatpak --user remote-add --no-gpg-verify local-repo local-repo"
 
 for th in ${THEME_VARIANTS}; do
+  # Show - build in local-repo
+  echo "flatpak-builder --repo=local-repo --force-clean build ${FLATPAK_PREFIX}.${th}.json"
+
   # Show local install
   echo "flatpak --user install local-repo ${FLATPAK_PREFIX}.${th} -y"
 done
